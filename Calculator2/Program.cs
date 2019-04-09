48985516df35b953f23af63ce0981ef37da378f4
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-04-08 23:39:10 -0500

    Completed and test passed

--- Calculator2/Program.cs
+++ Calculator2/Program.cs
@@ -4,18 +4,80 @@ namespace Calculator2
 {
     public static class Program
     {
+        
+
         public static void Main()
         {
-            double result = 0;
+            Console.Write("Please enter your first number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
 
-            // Create a simple calculator that asks the user to input two different numbers 
-            // and whether they want to add, subtract, multiply or divide them. 
+            Console.Write("Please enter your second number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
 
-            // Use methods for each type of calculation.
+            Console.WriteLine("Would you like to add, subtract, multiply, or divide? ");
+            string operation = Console.ReadLine();
 
-            Console.WriteLine($"Total = {result}");
+            if (operation == "add")
+            {
+                Add(firstNumber, secondNumber);
+                Console.WriteLine($"Total = {Add(firstNumber, secondNumber)}");
+            }
+            
+            if (operation == "subtract")
+            {
+                Subtract(firstNumber, secondNumber);
+                Console.WriteLine($"Total = {Subtract(firstNumber, secondNumber)}");
+            }
+
+            if (operation == "multiply")
+            {
+                Multiply(firstNumber, secondNumber);
+                Console.WriteLine($"Total = {Multiply(firstNumber, secondNumber)}");
+            }
+
+            if (operation == "divide")
+            {
+                Divide(firstNumber, secondNumber);
+                Console.WriteLine($"Total = {Divide(firstNumber, secondNumber)}");
+            }
+
 
             Console.ReadLine();
         }
+
+        // Use methods for each type of calculation.
+        public static double Add(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber + secondNumber;
+            
+            return result;
+        }
+        
+        public static double Subtract(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber - secondNumber;
+
+            return result;
+        }
+
+        public static double Multiply(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber * secondNumber;
+
+            return result;
+        }
+
+        public static double Divide(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber / secondNumber;
+
+            return result;
+        }
+
+        
+
+
+
+
     }
 }
